fd3b302e8726e0f9b8d3bcd3b40e1da6d2582e33
ourangelofsorrows <angelezrameyer@gmail.com>
2019-02-19 16:35:16 +0100

    fiddled around with js to get the on hover to work, but I need a teacher

--- Website/Code/js/js.js
+++ Website/Code/js/js.js
@@ -2,7 +2,7 @@ const main=document.querySelector("#posters");
 const template=document.querySelector("#poster-template").content;
 const link = "https://spreadsheets.google.com/feeds/list/1Zai3AoYR3yQ7zoGiXeiOVQnmLlf3vQjiQ_ELSgVlJkg/od6/public/values?alt=json";
 const modal =document.querySelector("#modal");
-const hoverinfo =document.querySelector("#hoverinfo");
+
 
 
 function loadJSON(link) {
@@ -16,15 +16,18 @@ function displayTarData(data){
     clone.querySelector("#poster").src=data.gsx$movieposter.$t;
     document.querySelector("#posters").addEventListener("click", hoverInfoShow);
 
+    document.querySelector("#hoverinfo").addEventListener("click", ()=>{
+        fetch(link+data.id).then(e=>e.json()).then(data=>hoverInfoShow(data));
+            });
 
-    function hoverInfoShow(data){
+    function hoverInfoShow(film){
         hoverinfo.classList.remove("inactive");
-        hoverinfo.querySelector("#imdb").textContent=data.gsx$imdb.$t;
-        hoverinfo.querySelector("#title").textContent=data.gsx$title.$t;
-        hoverinfo.querySelector("#age-rating").textContent=data.gsx$ageRating.$t;
-        hoverinfo.querySelector("#year").textContent=data.gsx$releaseYear.$t;
-        hoverinfo.querySelector("#genres").textContent=data.gsx$genres.$t;
-        hoverinfo.querySelector("#short description").textContent=data.gsx$leadroles.$t;
+        hoverinfo.querySelector("#imdb").textContent=film.gsx$imdb.$t;
+        hoverinfo.querySelector("#title").textContent=film.gsx$title.$t;
+        hoverinfo.querySelector("#age-rating").textContent=film.gsx$ageRating.$t;
+        hoverinfo.querySelector("#year").textContent=film.gsx$releaseYear.$t;
+        hoverinfo.querySelector("#genres").textContent=film.gsx$genres.$t;
+        hoverinfo.querySelector("#short description").textContent=film.gsx$leadroles.$t;
     
         
     }
