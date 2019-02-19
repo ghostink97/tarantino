04cdfdb0f60f20210a25690cdb5b72573b297996
ourangelofsorrows <angelezrameyer@gmail.com>
2019-02-19 16:17:18 +0100

    Been trying to get the hover to work, but I am getting a mistake.

--- Website/Code/js/js.js
+++ Website/Code/js/js.js
@@ -1,6 +1,9 @@
 const main=document.querySelector("#posters");
 const template=document.querySelector("#poster-template").content;
 const link = "https://spreadsheets.google.com/feeds/list/1Zai3AoYR3yQ7zoGiXeiOVQnmLlf3vQjiQ_ELSgVlJkg/od6/public/values?alt=json";
+const modal =document.querySelector("#modal");
+const hoverinfo =document.querySelector("#hoverinfo");
+
 
 function loadJSON(link) {
     fetch(link).then(e=>e.json()).then(data=>data.feed.entry.forEach(displayTarData));
@@ -11,8 +14,24 @@ function displayTarData(data){
     const clone=template.cloneNode("true");
 
     clone.querySelector("#poster").src=data.gsx$movieposter.$t;
+    document.querySelector("#posters").addEventListener("click", hoverInfoShow);
+
+
+    function hoverInfoShow(data){
+        hoverinfo.classList.remove("inactive");
+        hoverinfo.querySelector("#imdb").textContent=data.gsx$imdb.$t;
+        hoverinfo.querySelector("#title").textContent=data.gsx$title.$t;
+        hoverinfo.querySelector("#age-rating").textContent=data.gsx$ageRating.$t;
+        hoverinfo.querySelector("#year").textContent=data.gsx$releaseYear.$t;
+        hoverinfo.querySelector("#genres").textContent=data.gsx$genres.$t;
+        hoverinfo.querySelector("#short description").textContent=data.gsx$leadroles.$t;
+    
+        
+    }
 
     main.appendChild(clone);
 }
 
 loadJSON(link);
+
+
